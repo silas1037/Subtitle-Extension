5a0a7759b1ac5dff58546e8d40b75d9a81bd5633
wsl98501 <32613953+wsl98501@users.noreply.github.com>
2019-11-28 00:37:50 +0900

    update

--- ExampleExtension/Extension.cpp
+++ ExampleExtension/Extension.cpp
@@ -1,11 +1,126 @@
 #include "Extension.h"
+#include <stdio.h>
+#include <tchar.h>
+// Windows 头文件:
+#include <windows.h>
 
+// C 运行时头文件
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+//GDI库
+#include <GdiPlus.h>
+#pragma comment(lib,"GdiPlus.lib")
+
+#include <SDKDDKVer.h>
+
+using namespace Gdiplus;
+using namespace DllExports;
+
+//能够使用Visual C＋＋中的一些特殊COM支持类。有了这个头文件，gdiplus.h里面有东西就可以用了。
+//#include <comdef.h>
+
+//主窗口COMMAND处理
+HMODULE lrc;
+typedef void(*CreateLRC)();
+typedef void(*InitGDI)();
+typedef void(*ReleaseDrawLRC)();
+typedef void(*DrawLRC_SHOW_TEXT)(WCHAR *text);
+typedef void(*SetLRCColor)(int lrc_color);
+typedef void(*SetDrawLRCTime)(LONG time);
+typedef void(*LockingLRC)();
+
+DrawLRC_SHOW_TEXT draw_text;
+SetLRCColor setcolor;
+SetDrawLRCTime settime;
+LockingLRC lockingLRC;
+
+#ifdef _UNICODE
+#define COUT wcout
+#else
+#define COUT cout
+#endif
+
+std::hash<std::wstring> h;
+using namespace std;
+void inisub() {
+	lrc = LoadLibrary(_T("DrawLRC_DLL.dll"));
+	InitGDI in;
+	in = (InitGDI)GetProcAddress(lrc, "InitGDI");
+	in();
+	CreateLRC mb;
+	mb = (CreateLRC)GetProcAddress(lrc, "CreateLRC");
+	mb();
+	draw_text = (DrawLRC_SHOW_TEXT)GetProcAddress(lrc, "DrawLRC_SHOW_TEXT");
+	setcolor = (SetLRCColor)GetProcAddress(lrc, "SetLRCColor");
+	settime = (SetDrawLRCTime)GetProcAddress(lrc, "SetDrawLRCTime");
+	lockingLRC = (LockingLRC)GetProcAddress(lrc, "LockingLRC");
+}
+
+void putsub(LPCWSTR Ltext) {
+	WCHAR ch[1000] = { '\0' };
+	wcscpy_s(ch, Ltext);
+	WCHAR * text = ch;
+	draw_text(text);
+}
+static void make_console() {
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONIN$", "r", stdin);
+	std::cout << "debug" << std::endl;
+}
+map <size_t, LPCWSTR> m1;
+int mark = 0;
+bool loadText()
+{
+
+	const std::locale empty_locale = std::locale::empty();
+	typedef std::codecvt_utf8<wchar_t> converter_type;  //std::codecvt_utf16
+	const converter_type* converter = new converter_type;
+	const std::locale utf8_locale = std::locale(empty_locale, converter);
+
+	std::wifstream fin1("jpdic.txt");  //input
+	fin1.imbue(utf8_locale);
+	std::wifstream fin2("zhdic.txt");  //input
+	fin2.imbue(utf8_locale);
+	std::wstring line1;
+	std::wstring line2;
+
+	int idx = 0;
+	while (!fin1.eof() && !fin2.eof())
+	{
+		std::getline(fin1, line1);
+
+		line1 = line1.append(L"\n");
+
+		//CString str1(line1.c_str());
+		std::getline(fin2, line2);
+		CString str2(line2.c_str());
+
+		LPCWSTR lpcwStr = str2.AllocSysString();
+		m1.insert(pair<size_t, LPCWSTR>(h(line1), lpcwStr));
+		//printf("%s\n", (LPCTSTR)str);
+		idx++;
+	}
+	fin1.close();
+	fin2.close();
+
+	cout << "Load Texts:0x" << hex << idx << endl;
+	return true;
+}
 BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
 		MessageBoxW(NULL, L"Extension Loaded", L"Example", MB_OK);
+		make_console();
+		loadText();
+		inisub();
+		putsub(L"");
 		break;
 	case DLL_PROCESS_DETACH:
 		MessageBoxW(NULL, L"Extension Removed", L"Example", MB_OK);
@@ -26,9 +141,24 @@ BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved
 	This function may be run concurrently with itself: please make sure it's thread safe.
 	It will not be run concurrently with DllMain.
 */
+
+
 bool ProcessSentence(std::wstring& sentence, SentenceInfo sentenceInfo)
 {
-	// Your code here...
+	
+
+	setlocale(LC_ALL, "chs");
+	
+	LPCWSTR resl = m1[h(sentence)];
+	if (resl != NULL) {
+		wcout << resl << endl;
+		putsub(resl);
+		//WinExec("PutSub.exe 测试",0);
+		///delete[]resultStr;
+	}
+
+	
+	return true;
 #ifdef COPY_CLIPBOARD
 	// This example extension automatically copies sentences from the hook currently selected by the user into the clipboard.
 	if (sentenceInfo["current select"])
--- ExampleExtension/Extension.h
+++ ExampleExtension/Extension.h
@@ -4,7 +4,30 @@
 #include <windows.h>
 #include <cstdint>
 #include <string>
+#include <locale>
+#include <codecvt>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+//#include <hash_map>
+#include <stdio.h>
+#include <tchar.h>
+#include <functional>
+#include <string>
 
+#include <iostream>
+#include <mutex>
+#include "tchar.h"
+#include <string>
+#include <map>
+#include <vector>
+#include <atlstr.h>
+
+#include <locale>
 
 struct InfoForExtension
 {
